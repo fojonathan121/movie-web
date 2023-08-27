50bc7c8215e976a2d4581858d3af8bd97ad51941
Meowflow <84283465+fojonathan121@users.noreply.github.com>
2023-08-26 20:02:08 -0500

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_CORS_PROXY_URL: "https://xneomovies.fosshoss2.workers.dev/",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOGYxYmExMDJlZjk1N2RhZTRkNjg1ODBhN2U0MDkwNyIsInN1YiI6IjY0ZWEzOWI3MWZlYWMxMDEzOGRhZWMwMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6SFlXaO0Y2h3yWZR74TNamBFjoj7vJM260gFF7u4FQI"
 };
